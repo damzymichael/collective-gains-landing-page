956ea81054176685b3981fe04ce2d8d096976360
damzymichael <105460672+damzymichael@users.noreply.github.com>
2025-10-29 15:39:29 +0100

    Dashboard url added to env

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 // @ts-check
-import { defineConfig } from "astro/config"
+import { defineConfig, envField } from "astro/config"
 import react from "@astrojs/react"
 import tailwindcss from "@tailwindcss/vite"
 
-import vercel from "@astrojs/vercel";
+import vercel from "@astrojs/vercel"
 
 // https://astro.build/config
 export default defineConfig({
---
+++ env.d.ts
@@ -0,0 +1,7 @@
+interface ImportMetaEnv {
+  readonly PUBLIC_DASHBOARD_URL: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
